9630e81335cf9e0169122fd2765f0cbf17a2d16b
ThomasVancampen <thomas.vancampen@hotmail.com>
2023-01-13 13:18:51 +0100

    fully functional crud app

--- Client/src/reducers/posts.js
+++ Client/src/reducers/posts.js
@@ -1,12 +1,16 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
-            console.log("call")
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
-        case 'UPDATE':
-            return posts.map((post) => posts._id == action.payload._id ? action.payload : post);
+        case UPDATE:
+        case LIKE:
+            return posts.map((post) => post._id === action.payload._id ? action.payload : post);
+        case DELETE:
+            return posts.filter((post) => post._id !== action.payload);
         default:
             return posts;
     }
