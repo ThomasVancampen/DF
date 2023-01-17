e6e5db9d5fcbd4de17794f0c7d2a8bc4a207dd82
ThomasVancampen <thomas.vancampen@hotmail.com>
2023-01-17 17:50:23 +0100

    search and pagination

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { gapi } from "gapi-script";
 
+import PostDetails from './components/PostDetails/PostDetails';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
@@ -16,16 +17,23 @@ gapi.load("client:auth2", () => {
     });
   });
 
-const App = () => (
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
+
+  return (
     <BrowserRouter>
-        <Container maxwidth="lg">
+        <Container maxwidth="xl">
             <Navbar />
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/auth" exact component={Auth} />
+                <Route path="/" exact component={() => <Redirect to="/posts" />} />
+                <Route path="/posts" exact component={Home} />
+                <Route path="/posts/search" exact component={Home} />
+                <Route path="/posts/:id" component={PostDetails} />
+                <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts"/>)} />
             </Switch>
         </Container>
     </BrowserRouter>
-);
+  )
+}
 
 export default App;
